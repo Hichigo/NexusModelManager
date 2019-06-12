e5f30975c12ef3e128784b0ab79c9af9199d0ce0
Ivan <hichigo91@gmail.com>
2019-06-12 18:21:49 +0300

    create tools for background append collection to new blend file

---
+++ tools/create_asset.py
@@ -0,0 +1,20 @@
+import bpy
+import sys
+import os
+
+if __name__ == '__main__':
+    append_from_blendfile = sys.argv[5]
+    directory_folder = sys.argv[6] # in my case "Collection"
+    collection_name = sys.argv[7]
+    save_dir = sys.argv[8]
+
+    save_file = os.path.join(save_dir, collection_name + ".blend")
+
+    filepath = os.path.join(append_from_blendfile, directory_folder, collection_name)
+    directory = os.path.join(append_from_blendfile, directory_folder)
+
+    bpy.ops.wm.append(filepath=filepath, directory=directory, filename=collection_name)
+
+    bpy.ops.wm.save_as_mainfile(filepath=save_file)
+
+    bpy.ops.wm.quit_blender()
