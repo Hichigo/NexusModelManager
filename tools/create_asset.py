3b00cc4cdc12b0257cb335d30e81bd424cc51397
Ivan <hichigo91@gmail.com>
2019-06-13 20:43:58 +0300

    add to empty object asset

--- tools/create_asset.py
+++ tools/create_asset.py
@@ -13,7 +13,26 @@ if __name__ == '__main__':
     filepath = os.path.join(append_from_blendfile, directory_folder, collection_name)
     directory = os.path.join(append_from_blendfile, directory_folder)
 
+    # create empty object
+    bpy.ops.object.empty_add(type='PLAIN_AXES')
+    empty_object = bpy.data.objects['Empty']
+    empty_object.name = collection_name
+
+    # append asset
     bpy.ops.wm.append(filepath=filepath, directory=directory, filename=collection_name, autoselect=True)
+
+    # set empty object active
+    empty_object.select_set(True)
+    bpy.context.window.view_layer.objects.active = empty_object
+
+    # link empty object to collection
+    bpy.data.collections[collection_name].objects.link(empty_object)
+
+    # unlink empty object from root collection
+    bpy.context.scene.collection.objects.unlink(empty_object)
+
+    # set parent all selected object to empty object
+    bpy.ops.object.parent_set(type='OBJECT', keep_transform=False)
 
     # save file
     bpy.ops.wm.save_as_mainfile(filepath=save_file)
