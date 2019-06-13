fa511f8d050e5b4ce545ba20dfbc773a41a9fce5
Ivan <hichigo91@gmail.com>
2019-06-13 20:44:22 +0300

    mesh paint only link model

--- functions.py
+++ functions.py
@@ -16,11 +16,7 @@ def add_model(context, location, normal):
     category = nexus_model_SCN.category_list
     library = nexus_model_SCN.library_list
     collection_name = nexus_model_SCN.collection_asset
-    is_link = nexus_model_SCN.link_model
-    inst_collections = nexus_model_SCN.instance_collections
-    add_dupli_to_sel = nexus_model_SCN.add_duplicollection
     collection_or_meshdata = nexus_model_SCN.collection_or_meshdata
-    set_to_selected_objects = nexus_model_SCN.set_to_selected_objects
 
     filepath = os.path.join(path_models, library, category, filename, filename + ".blend")
 
@@ -37,30 +33,16 @@ def add_model(context, location, normal):
 
     filepath_collection_name = directory_inside_file + collection_name
 
-
-    #selected_objects = context.selected_objects
-
-    #if not add_dupli_to_sel:
     bpy.ops.object.select_all(action="DESELECT")
 
-    if is_link:
-        bpy.ops.wm.link(
-            filepath=filepath_collection_name,
-            filename=collection_name,
-            directory=directory_inside_file,
-            link=True,
-            instance_collections=inst_collections,
-            autoselect=True
-        )
-    else:
-        bpy.ops.wm.append(
-            filepath=filepath_collection_name,
-            filename=collection_name,
-            directory=directory_inside_file,
-            link=False,
-            instance_collections=False,
-            autoselect=True
-        )
+    bpy.ops.wm.link(
+        filepath=filepath_collection_name,
+        filename=collection_name,
+        directory=directory_inside_file,
+        link=True,
+        instance_collections=True,
+        autoselect=True
+    )
     
     mat_trans = Matrix.Translation(location) # location matrix
     mat_rot = normal.to_track_quat("Z","Y").to_matrix().to_4x4() # rotation matrix
