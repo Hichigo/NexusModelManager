50c9a0fa892b7c43f57399e941537b7d4ee37633
Ivan <hichigo91@gmail.com>
2019-06-14 17:56:32 +0300

    move asset to center

--- tools/create_asset.py
+++ tools/create_asset.py
@@ -1,12 +1,15 @@
 import bpy
 import sys
 import os
+from mathutils import Vector
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     append_from_blendfile = sys.argv[5]
     directory_folder = sys.argv[6] # in my case "Collection"
     collection_name = sys.argv[7]
     save_dir = sys.argv[8]
+    offset_location = sys.argv[9].split("|") # split string by "|" symbol
+    offset_location = Vector( (float(offset_location[0]), float(offset_location[1]), float(offset_location[2])) ) # make vector
 
     save_file = os.path.join(save_dir, collection_name + ".blend")
 
@@ -14,12 +17,17 @@ if __name__ == '__main__':
     directory = os.path.join(append_from_blendfile, directory_folder)
 
     # create empty object
-    bpy.ops.object.empty_add(type='PLAIN_AXES')
-    empty_object = bpy.data.objects['Empty']
+    bpy.ops.object.empty_add(type="PLAIN_AXES")
+    empty_object = bpy.data.objects["Empty"]
     empty_object.name = collection_name
 
     # append asset
     bpy.ops.wm.append(filepath=filepath, directory=directory, filename=collection_name, autoselect=True)
+
+    # move all selected objects to center scene
+    selected_objects = bpy.context.selected_objects
+    for obj in selected_objects:
+        obj.location -= offset_location
 
     # set empty object active
     empty_object.select_set(True)
@@ -32,7 +40,7 @@ if __name__ == '__main__':
     bpy.context.scene.collection.objects.unlink(empty_object)
 
     # set parent all selected object to empty object
-    bpy.ops.object.parent_set(type='OBJECT', keep_transform=False)
+    bpy.ops.object.parent_set(type="OBJECT", keep_transform=False)
 
     # set view camera to selected objects
     bpy.ops.view3d.camera_to_view_selected()
@@ -45,7 +53,10 @@ if __name__ == '__main__':
     bpy.ops.render.render(write_still=True)
 
     # remove camera
-    bpy.data.objects.remove(bpy.data.objects['Camera'])
+    # bpy.data.objects.remove(bpy.data.objects["Camera"])
+
+    # remove HDRI image
+    bpy.data.images.remove(bpy.data.images["tomoco_studio.exr"])
 
     # save file
     bpy.ops.wm.save_as_mainfile(filepath=save_file)
