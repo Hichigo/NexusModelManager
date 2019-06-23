d066bd7848cc43aaaca070e75d13576a006c34c3
Ivan <hichigo91@gmail.com>
2019-06-23 16:39:18 +0300

    create file for ui and create class ui list

---
+++ ui.py
@@ -0,0 +1,13 @@
+import bpy
+
+class STRING_UL_RandomAssets(bpy.types.UIList):
+
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
+        name = item.name
+
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            layout.label(text=name, icon_value=icon)
+            layout.prop(item, "path_to_asset", text="")
+        # elif self.layout_type in {'GRID'}:
+        #     layout.alignment = 'CENTER'
+        #     layout.label(text="", icon_value=icon)
