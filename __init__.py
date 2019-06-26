a153da14b194feaec92ae9e1f253517da59365b9
Ivan <hichigo91@gmail.com>
2019-06-26 11:53:17 +0300

    moved code to files

--- __init__.py
+++ __init__.py
@@ -8,15 +8,14 @@ bl_info = {
 	"warning": "",
 	"wiki_url": "https://github.com/Hichigo/NexusModelManager",
 	"category": "Object"
-	}
+}
 
 import bpy
 import os
-import glob
 
 from bpy.props import *
 import bpy.utils.previews
-from bpy.types import AddonPreferences, WindowManager, Panel, Operator, PropertyGroup
+from bpy.types import AddonPreferences, WindowManager, Panel, Operator
 
 from .CreateAsset.create_asset_op import *
 from .CreateAsset.create_asset_pt import *
@@ -24,127 +23,12 @@ from .CreateAsset.create_asset_pt import *
 from .MeshPaint.mesh_paint_op import *
 from .MeshPaint.mesh_paint_pt import *
 
+from .properties import *
+
 from .random_list_ui import *
 
-from .functions import get_addon_dir, get_addon_prefs
+from .functions import get_addon_prefs
 
-def filter_on_mesh_prop(self, object):
-    return object.type == "MESH"
-
-def enum_render_scenes(self, context):
-	scenes_list = []
-	addon_folder = get_addon_dir()
-	addon_prefs = get_addon_prefs()
-	scenes_dir = os.path.join(addon_prefs.path_to_render_scenes, "*.blend")
-	directory_icons = os.path.join(addon_prefs.path_to_render_scenes, "cameras_preview")
-
-	if context is None:
-		return scenes_list
-
-	pcoll = render_scene_collections["main"]
-
-	if directory_icons == pcoll.render_scene_previews_dir:
-		return pcoll.render_scene
-
-	scene_files_path = glob.glob(scenes_dir)
-
-	for i, scene_file_path in enumerate(scene_files_path):
-		if os.path.isfile(scene_file_path):
-			file_name = os.path.split(scene_file_path)[1] # get file name without path
-			file_name = file_name.split(".")[0] # get file name without extension
-			icon_path = os.path.join(directory_icons, file_name + ".png")
-			
-			if icon_path in pcoll:
-				scenes_list.append((scene_file_path, file_name, "", pcoll[icon_path].icon_id, i))
-			else:
-				thumb = pcoll.load(icon_path, icon_path, "IMAGE")
-				scenes_list.append((scene_file_path, file_name, "", thumb.icon_id, i))
-
-	scenes_list.sort()
-
-	pcoll.render_scene = scenes_list
-	pcoll.render_scene_previews_dir = directory_icons
-	return pcoll.render_scene
-
-render_scene_collections = {}
-
-###########################################################################
-#################### function dinamicaly make category ####################
-###########################################################################
-
-def make_list_folder(path):
-
-	dirs = os.listdir(path)
-	i = 0
-	mode_options = []
-
-	for dir in dirs:
-		if dir != "textures":
-			if os.path.isdir(os.path.join(path, dir)):
-				item = (dir, dir, "", i)
-				mode_options.append(item)
-				i += 1
-
-	return mode_options
-
-############################ Category ##########################
-def make_category_list(self, context):
-	library_dir = context.window_manager.nexus_model_manager_dir_resource
-	library = context.scene.nexus_model_manager.library_list
-	path_category = os.path.join(library_dir, library)
-
-	return make_list_folder(path_category)
-
-############################ Library ##########################
-def make_library_list(self, context):
-	library_dir = context.window_manager.nexus_model_manager_dir_resource
-
-	return make_list_folder(library_dir)
-
-##################################################################
-############################ Previews ############################
-##################################################################
-
-######### Assets Previews ##########
-
-def enum_previews_asset_items(self, context):
-	""" create assets items prewiews """
-	enum_items = []
-
-	library_dir = bpy.data.window_managers["WinMan"].nexus_model_manager_dir_resource
-	category = context.scene.nexus_model_manager.category_list
-	library = context.scene.nexus_model_manager.library_list
-	directory = os.path.join(library_dir, library, category)
-
-
-	if context is None:
-		return enum_items
-
-	pcoll = asset_collections["main"]
-
-	if directory == pcoll.asset_previews_dir:
-		return pcoll.asset_previews
-
-	if directory and os.path.exists(directory):
-		assets_names = []
-		for fn in os.listdir(directory):
-			assets_names.append(fn)
-
-		for i, name in enumerate(assets_names):
-			filepath = os.path.join(directory, name, "render", name + ".png")
-
-			if filepath in pcoll:
-				enum_items.append((name, name, "", pcoll[filepath].icon_id, i))
-			else:
-				thumb = pcoll.load(filepath, filepath, "IMAGE")
-				enum_items.append((name, name, "", thumb.icon_id, i))
-	enum_items.sort()
-
-	pcoll.asset_previews = enum_items
-	pcoll.asset_previews_dir = directory
-	return pcoll.asset_previews
-
-asset_collections = {}
 
 ###########################################################################
 ############################ Addon preferences ############################
@@ -547,145 +431,6 @@ class SCENE_OT_RemoveListItem(Operator):
 		random_asset_list.list_item.remove(remove_index)
 
 		return {'FINISHED'}
-
-class NexusModelManager_WM_Properties(PropertyGroup):
-
-	canvas_object: PointerProperty(
-		name="Canvas",
-		description="If empty then will draw over all objects. APPLY TRANSFORM TO OBJECT!!!",
-        type=bpy.types.Object,
-        poll=filter_on_mesh_prop
-    )
-
-	use_random_asset: BoolProperty(
-		name="Use random asset",
-		description="If True get asset from list",
-		default=False
-	)
-
-	new_collection_name: StringProperty(
-		name="Collection",
-		description="Name of New Collection",
-		default="Awesome_Collection"
-	)
-
-	align_by_normal: BoolProperty(
-		name="Align by Normal",
-		description="Align asset by normal",
-		default=True
-	)
-
-	apply_cursor_rotation: BoolProperty(
-		name="Apply 3D Cursor Rotation",
-		description="Apply 3D Cursor Rotation to New Asset",
-		default=True
-	)
-
-	link_model: BoolProperty(
-		name="Link",
-		description="If True link model else append model",
-		default=True
-	)
-
-	set_to_selected_objects: BoolProperty(
-		name="Set to selected objects",
-		description="Set mesh data to selected objects",
-		default=False
-	)
-
-	distance_between_asset: FloatProperty(
-		name="Distance",
-		description="Distance to which assets can be placed",
-		min=0,
-		default=2
-	)
-
-	use_random_rotation: BoolProperty(
-		name="Use random rotation",
-		description="Set random rotation to asset",
-		default=False
-	)
-
-	random_rotation_x: FloatProperty(
-		name="X",
-		description="Random rotation by X",
-		min=0,
-		max=359,
-		default=0
-	)
-	random_rotation_y: FloatProperty(
-		name="Y",
-		description="Random rotation by Y",
-		min=0,
-		max=359,
-		default=0
-	)
-	random_rotation_z: FloatProperty(
-		name="Z",
-		description="Random rotation by Z",
-		min=0,
-		max=359,
-		default=0
-	)
-
-	use_random_scale: BoolProperty(
-		name="Use random scale",
-		description="Set random scale to asset",
-		default=False
-	)
-	random_scale_from: FloatProperty(
-		name="From",
-		description="Random scale from",
-		min=0,
-		default=1
-	)
-	random_scale_to: FloatProperty(
-		name="To",
-		description="Random scale to",
-		min=0,
-		default=2
-	)
-
-	asset_previews: EnumProperty(
-		items=enum_previews_asset_items
-	)
-
-	library_list: EnumProperty(
-		items=make_library_list
-	)
-
-	category_list: EnumProperty(
-		items=make_category_list
-	)
-
-	render_scenes: EnumProperty(
-		items=enum_render_scenes
-	)
-
-	pack_data: EnumProperty(
-		name="Pack data",
-		items=[
-			("NONE", "None", "", 0),
-			("PACK", "Pack to .blend", "", 1),
-			("TEXTURE", "Texture to library", "", 2)
-		],
-		default = "NONE"
-	)
-
-	add_location: EnumProperty(
-		name="Add location",
-		items=[
-			("CENTER", "Center", "", 0),
-			("CURSOR", "Cursor", "", 1)
-		],
-		default = "CENTER"
-	)
-
-	add_duplicollection: BoolProperty(
-		name="Add duplicollection to selected",
-		description="Add duplicollection to selected objects",
-		default=False
-	)
 
 ######################################################################
 ############################## Register ##############################
