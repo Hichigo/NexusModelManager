e0f55dc7867ca560d3bd2bbf7b80c05955de81ee
Ivan <hichigo91@gmail.com>
2019-06-26 13:51:33 +0300

    moved code to files and import this files

--- __init__.py
+++ __init__.py
@@ -20,6 +20,9 @@ from bpy.types import AddonPreferences, WindowManager, Panel, Operator
 from .CreateAsset.create_asset_op import *
 from .CreateAsset.create_asset_pt import *
 
+from .PreviewAssets.preview_assets_op import *
+from .PreviewAssets.preview_assets_pt import *
+
 from .MeshPaint.mesh_paint_op import *
 from .MeshPaint.mesh_paint_pt import *
 
@@ -91,178 +94,9 @@ class VIEW3D_PT_MainPanel(Panel):
 		col.prop(wm, "nexus_model_manager_dir_resource")
 		col.operator("view3d.library_path", icon="FILE_FOLDER", text="Open Library Folder")
 
-class VIEW3D_PT_ManagerPreviews(Panel):
-
-	bl_label = "Model Manager"
-	bl_idname = "VIEW3D_PT_ManagerPreviews"
-	bl_space_type = "VIEW_3D"
-	bl_region_type = "UI"
-	bl_category = "Nexus"
-	bl_parent_id = "VIEW3D_PT_MainPanel"
-
-	@classmethod
-	def poll(cls, context):
-		return context.mode == "OBJECT"
-
-	def draw(self, context):
-		addon_prefs = get_addon_prefs()
-		layout = self.layout
-		wm = context.window_manager
-		nexus_model_SCN = context.scene.nexus_model_manager
-
-		library_dir = context.window_manager.nexus_model_manager_dir_resource
-		asset_name = os.path.splitext(nexus_model_SCN.asset_previews)[0]
-		category = nexus_model_SCN.category_list
-		library = nexus_model_SCN.library_list
-
-		render_path = os.path.join(library_dir, library, category, asset_name, "render")
-
-############## Panel ##############
-
-		box = layout.box()
-		box.label(text="Model Manager")
-
-####### Drop Down Menu library
-		row = box.row()
-		row.label(text="Library")
-		row.prop(nexus_model_SCN, "library_list", text="")
-
-####### Drop Down Menu category
-		row = box.row()
-		row.label(text="Category")
-		row.prop(nexus_model_SCN, "category_list", text="")
-
-####### Asset folder button
-		col = box.column()
-		row = col.row(align=True)
-		row.operator("view3d.asset_path", icon="FILE_FOLDER", text="Open Asset Folder")
-		row.operator("view3d.image_path", icon="FILE_IMAGE", text="Open Image")
-
-####### Previews
-		col = box.column()
-		col.scale_y = 1.5
-		col.template_icon_view(nexus_model_SCN, "asset_previews", show_labels=True, scale_popup=addon_prefs.preview_asset_scale)
-		# col.template_ID_preview(asset_collections["main"].asset_previews, "name", rows=3, cols=8)
-		# col.template_search_preview(nexus_model_SCN, "asset_previews", search_data, "name")
-		col.label(text=asset_name)
-
-		row = box.row()
-		row.operator("view3d.search_asset", icon="VIEWZOOM", text="Search Asset")
-
-# ####### Add location
-		row = box.row()
-		row.label(text="Add location")
-		row = box.row()
-		row.prop(nexus_model_SCN, "add_location", expand=True)
-
-####### link and dupli
-		col = box.column()
-		row = col.row()
-		row.prop(nexus_model_SCN, "link_model")
-		row = col.row()
-		row.enabled = nexus_model_SCN.link_model
-		row.prop(nexus_model_SCN, "add_duplicollection")
-
-####### Add Button
-		col = box.column(align=True)
-		col.operator("view3d.add_model", icon="ADD", text="Add Asset")
-
 ################################################################
 ############################ Append ############################
 ################################################################
-
-class VIEW3D_OT_AddModel(Operator):
-
-	bl_idname = "view3d.add_model"
-	bl_label = "Add Model?"
-
-	# def draw(self, context):
-	# 	layout = self.layout
-	# 	col = layout.column()
-	# 	col.label("The asset is already added. Add more?")
-
-	# def invoke(self, context, event):
-	# 	nexus_model_SCN = context.scene.nexus_model_manager
-	# 	collection_name = nexus_model_SCN.asset_previews
-	# 	is_link = nexus_model_SCN.link_model
-	# 	add_dupli_to_sel = nexus_model_SCN.add_duplicollection
-
-	# 	if not is_link and add_dupli_to_sel and nexus_model_SCN.add_location == "CURSOR":
-	# 		self.report({"INFO"}, "Set Add location to Center")
-	# 		nexus_model_SCN.add_location = "CENTER"
-
-	# 	if bpy.data.collections.get(collection_name) is not None:
-	# 		bpy.ops.object.collection_instance_add(collection=collection_name)
-	# 		self.report({"INFO"}, "Added Intance collection from scene (allready exist in scene)")
-	# 		# return context.window_manager.invoke_props_dialog(self)
-	# 	else:
-	# 		self.execute(context)
-		
-	# 	return {"FINISHED"}
-
-
-	def execute(self, context):
-		nexus_model_SCN = context.scene.nexus_model_manager
-		library_dir = context.window_manager.nexus_model_manager_dir_resource
-		asset_name = nexus_model_SCN.asset_previews
-		category = nexus_model_SCN.category_list
-		library = nexus_model_SCN.library_list
-		is_link = nexus_model_SCN.link_model
-		add_dupli_to_sel = nexus_model_SCN.add_duplicollection
-		set_to_selected_objects = nexus_model_SCN.set_to_selected_objects
-
-		filepath = os.path.join(library_dir, library, category, asset_name, asset_name + ".blend")
-
-		directory_inside_file = os.path.join(filepath, "Collection")
-
-		filepath_collection_name = directory_inside_file + asset_name
-
-
-		selected_objects = context.selected_objects
-
-		# if not add_dupli_to_sel:
-		bpy.ops.object.select_all(action="DESELECT")
-
-		if is_link:
-			bpy.ops.wm.link(
-				filepath=filepath_collection_name,
-				filename=asset_name,
-				directory=directory_inside_file,
-				link=True,
-				instance_collections=True,
-				autoselect=True
-			)
-		else:
-			bpy.ops.wm.append(
-				filepath=filepath_collection_name,
-				filename=asset_name,
-				directory=directory_inside_file,
-				link=False,
-				instance_collections=False,
-				autoselect=True
-			)
-
-		if is_link:
-			if add_dupli_to_sel:
-				collection = bpy.data.collections[asset_name]
-				bpy.data.objects.remove(context.active_object)
-				for obj in selected_objects:
-					obj.instance_type = "COLLECTION"
-					obj.instance_collection = collection
-
-		if len(bpy.context.selected_objects) > 0:
-			if is_link:
-				if nexus_model_SCN.add_location == "CURSOR":
-					bpy.context.selected_objects[0].location = context.scene.cursor.location
-				else:
-					bpy.context.selected_objects[0].location = (0.0, 0.0, 0.0)
-			else:
-				if nexus_model_SCN.add_location == "CURSOR":
-					bpy.context.selected_objects[0].parent.location = context.scene.cursor.location
-				else:
-					bpy.context.selected_objects[0].parent.location = (0.0, 0.0, 0.0)
-
-		return {"FINISHED"}
 
 # class VIEW3D_OT_RemoveFolder(bpy.types.Operator):
 # 	"""Remove Folder"""
@@ -303,7 +137,7 @@ class VIEW3D_OT_AddModel(Operator):
 ######################################################################
 
 class VIEW3D_OT_LibraryPath(Operator):
-
+	"""Open folder library"""
 	bl_idname = "view3d.library_path"
 	bl_label = "Library Path"
 	
@@ -311,66 +145,6 @@ class VIEW3D_OT_LibraryPath(Operator):
 		library_dir = context.window_manager.nexus_model_manager_dir_resource
 		bpy.ops.wm.path_open(filepath=library_dir)
 		return {"FINISHED"}
-
-######################################################################
-############################ Asset path ############################
-######################################################################
-
-class VIEW3D_OT_AssetPath(Operator):
-
-	bl_idname = "view3d.asset_path"
-	bl_label = "Library Asset Path"
-
-	def execute(self, context):
-
-		nexus_model_SCN = context.scene.nexus_model_manager
-		model_dir = context.window_manager.nexus_model_manager_dir_resource
-		library = nexus_model_SCN.library_list
-		category = nexus_model_SCN.category_list
-		selected_preview = nexus_model_SCN.asset_previews
-
-		filepath = os.path.join(model_dir, library, category, selected_preview)
-
-		bpy.ops.wm.path_open(filepath=filepath)
-		return {"FINISHED"}
-
-class VIEW3D_OT_ImagePath(Operator):
-
-	bl_idname = "view3d.image_path"
-	bl_label = "Library Image Path"
-
-	def execute(self, context):
-
-		nexus_model_SCN = context.scene.nexus_model_manager
-		library_dir = context.window_manager.nexus_model_manager_dir_resource
-		library = nexus_model_SCN.library_list
-		category = nexus_model_SCN.category_list
-		asset_name = nexus_model_SCN.asset_previews
-
-		render_path = os.path.join(library_dir, library, category, asset_name, "render", asset_name + ".png")
-
-		bpy.ops.wm.path_open(filepath=render_path)
-		return {"FINISHED"}
-
-class VIEW3D_OT_SearchAsset(Operator):
-	bl_idname = "view3d.search_asset"
-	bl_label = "Search Asset"
-	bl_property = "search_asset"
-
-	search_asset: EnumProperty(
-		name="Search Asset",
-		items=enum_previews_asset_items,
-	)
-
-	def execute(self, context):
-		nexus_model_SCN = context.scene.nexus_model_manager
-		nexus_model_SCN.asset_previews = self.search_asset
-		return {"FINISHED"}
-
-	def invoke(self, context, event):
-		context.window_manager.invoke_search_popup(self)
-		return {"RUNNING_MODAL"}
-
 
 ######################################################################
 ############################## Register ##############################
