39d6a1a27d5537145dd4dcf41970ce672b7787dc
Ivan <hichigo91@gmail.com>
2019-06-26 14:53:47 +0300

    import types

--- MeshPaint/random_list_ui.py
+++ MeshPaint/random_list_ui.py
@@ -1,7 +1,8 @@
 import bpy
 from bpy.props import *
+from bpy.types import UIList, PropertyGroup
 
-class STRING_UL_RandomAssets(bpy.types.UIList):
+class STRING_UL_RandomAssets(UIList):
 
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
         name = item.name
@@ -13,10 +14,10 @@ class STRING_UL_RandomAssets(bpy.types.UIList):
         #     layout.alignment = 'CENTER'
         #     layout.label(text="", icon_value=icon)
 
-class ListItem(bpy.types.PropertyGroup):
+class ListItem(PropertyGroup):
 	# name_asset: StringProperty(name="Name", default="Empty")
 	path_to_asset: StringProperty(name="Path to asset", default="")
 
-class UIList_WM_Properties(bpy.types.PropertyGroup):
+class UIList_WM_Properties(PropertyGroup):
     list_item: CollectionProperty(name="List item", type=ListItem)
     active_index: IntProperty(name="Active Index", default=-1)
